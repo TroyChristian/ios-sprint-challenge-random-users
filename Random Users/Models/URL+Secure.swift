aa1b79f12bf5acb819072b16427780df46ec81e8
Troy Christian <apostasy8@gmail.com>
2020-01-17 15:36:07 -0500

    Add URL+Secure

---
+++ Random Users/Models/URL+Secure.swift
@@ -0,0 +1,16 @@
+//
+//  URL+Secure.swift
+//  Random Users
+//
+//  Created by Lambda_School_Loaner_219 on 1/17/20.
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+extension URL {
+    var usingHTTPS: URL? {
+        guard var components = URLComponents(url: self, resolvingAgainstBaseURL: true) else { return nil }
+        components.scheme = "https"
+        return components.url
+    }
+}
