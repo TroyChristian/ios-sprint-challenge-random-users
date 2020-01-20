1fe9153568384b1bd375ac7b43715e1f0142786d
Troy Christian <apostasy8@gmail.com>
2020-01-20 15:43:13 -0500

    MVP

--- Random Users/Operations/ViewControllers/DetailViewController.swift
+++ Random Users/Operations/ViewControllers/DetailViewController.swift
@@ -10,21 +10,46 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var detailPicture: UIImageView!
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var first: UILabel!
+    
+    @IBOutlet weak var last: UILabel!
+    
+    @IBOutlet weak var email: UILabel!
+    
+    @IBOutlet weak var phoneNumber: UILabel!
+   
+    var user:User?
+    var imgData:Data?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateViews()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        guard let user = user else {return}
+        if let imgData = imgData {
+            detailPicture.image = UIImage(data:imgData)
+        }
+        titleLabel.text? = user.name.title
+        first.text? = user.name.first
+        last.text? = user.name.last
+        
+        email.text? = user.email
+        phoneNumber.text? = user.phone
+        
     }
-    */
+    
+
+
 
 }
